baad35470c062878dc76d5348dfc16d830d068f3
Johanna Petersson <jojanna.petersson@hotmail.com>
2022-02-28 16:11:37 +0100

    Update GasMileage.java
    Added psudocode

--- Excersises from Book/GasMileage.java
+++ Excersises from Book/GasMileage.java
@@ -7,8 +7,26 @@
 *combined miles per gallon obtained for all trips up to this point. All averaging calculations should
 *produce floating-point results. Use class Scanner and sentinel-controlled iteration to obtain the data
 *from the user.
-
+*
+*Psudocode
+*1. Initiate int milesDriven = 1, gallonsUsed 
+*2. Initiate float totalMilesPerGallon = 0, milesPerGallon
+*3. Initiat scanner-object
+*4. prompt user
+*5. 	print "How many miles driven for this trip? to exit enter -1"
+*6.		milesDriven = user input	
+*7. While milesDriven != -1
+*8.		print "How many gallons were used for this trip?"
+*9.		gallonsUsed = user input
+*10.	milesPerGallon = milesDriven / gallonsUsed
+*11.	totalMilesPerGallon += milesPerGallon
+*12.	print ""The miles per gallon for this trip was " + milesPerGallon + "and the total miles per gallon so far is" + totalMilesPerGallon"
+*13.	print "How many miles driven for this trip? to exit enter -1"
+*14.	milesDriven = user input
+*15. 
+*16.Print "The total miles per gallon for all trips was" + totalMilesPerGallon
 */
+import java.util.Scanner; // program uses class Scanner
 
 class GasMileage 
 {
