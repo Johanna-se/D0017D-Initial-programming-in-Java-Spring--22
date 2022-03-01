f0762b186a9f2e9f42475586ddda45df4935bd5e
Johanna Petersson <jojanna.petersson@hotmail.com>
2022-03-01 12:34:35 +0100

    Create SalesCommissionCalculator.java
    Added the text from the book.

---
+++ Excersises from Book/SalesCommissionCalculator.java
@@ -0,0 +1,22 @@
+/**
+*Exercise 3.19 From the book:
+*A large company pays its salespeople on a commission basis.
+*The salespeople receive $200 per week plus 9% of their gross sales for that week. For example, a
+*salesperson who sells $5,000 worth of merchandise in a week receives $200 plus 9% of $5,000, or
+*a total of $650. You’ve been supplied with a list of the items sold by each salesperson. The values of
+*these items are shown in Fig. 3.15. Develop a Java application that inputs one salesperson’s items
+*sold for last week and calculates and displays that salesperson’s earnings. There’s no limit to the number
+*of items that can be sold.
+* Figur 3.15:
+*	Item 	Value
+*	1 		239.99
+*	2 		129.75
+*	3 		99.95
+*	4 		350.89
+*/
+class SalesCommissionCalculator
+{
+	public static void main(String[] args)
+	{
+	}
+}
