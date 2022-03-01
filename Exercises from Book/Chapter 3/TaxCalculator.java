249985b5b14cdc6da9155b065ea7d58084220875
Johanna Petersson <jojanna.petersson@hotmail.com>
2022-03-01 15:56:48 +0100

    Create TaxCalculator.java
    added the exercise text from the book

---
+++ Exercises from Book/Chapter 3/TaxCalculator.java
@@ -0,0 +1,15 @@
+/**
+*Exercise 3.20 From the book:
+* Develop a Java application that determines the total tax for each of three
+* citizens. The tax rate is 15% for earnings up to 30,000 USD earned by each citizen and 20% for all
+* earnings in excess of that ceiling. You are given a list with the citizens’ names and their earnings in a
+* given year. Your program should input this information for each citizen, then determine and display
+* the citizen’s total tax. Use class Scanner to input the data.
+*/
+
+class TaxCalculator
+{
+	public static void main(String[] args)
+	{
+	}
+}
