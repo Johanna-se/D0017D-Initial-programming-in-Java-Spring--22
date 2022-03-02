8238bd8ac8892a2c8603d6e6a8267b9e2db0da48
Johanna Petersson <jojanna.petersson@hotmail.com>
2022-03-02 09:04:35 +0100

    Update TaxCalculator.java
    Added code

--- Exercises from Book/Chapter 3/TaxCalculator.java
+++ Exercises from Book/Chapter 3/TaxCalculator.java
@@ -10,7 +10,7 @@
 * initiate Scanner
 * initiate constants TAX_LOWER, TAX_HIGHER, TAX_BREAKING_POINT
 * initiate varibles taxPayerCounter = 0, higherTaxLevel, earnings, tax
-* while taxPayerCounter != 4 
+* while taxPayerCounter != 3 
 * 	taxPayerCounter add 1
 *	Print: "What is the earning of taxpayer " + taxPayerCounter
 *	ernings = user input
@@ -30,5 +30,44 @@ class TaxCalculator
 {
 	public static void main(String[] args)
 	{
+		//create Scanner to obtain input from command window
+		Scanner input = new Scanner(System.in);
+
+		//initiate canstants
+		final float TAX_LOWER = 0.15f; 
+		final float TAX_HIGHER = 0.2f;
+		final int TAX_BREAKING_POINT = 30000;
+
+		//initiate variabls
+		int higherTaxLevel;
+		int earnings;
+		int taxPayerCounter = 0;
+		float tax = 0;
+    
+		// processing phase
+		// loop until taxPayerCounter reaches 3
+		while (taxPayerCounter != 3) 
+		{
+			//Add one to tax payers
+			taxPayerCounter++;
+
+			// Ask for earnings of taxpayer #taxPayerCounter from user
+			System.out.print("What is the earning of taxpayer nummer " + taxPayerCounter + "?: ");
+			earnings = input.nextInt();
+
+			//if taxpayer only reaches the lower tax level
+			if (earnings <= TAX_BREAKING_POINT)
+			{
+				tax = (float) earnings * TAX_LOWER;
+				System.out.printf("Tax for taxpayer %d was %.2f.%n%n",  taxPayerCounter, tax );
+			}
+			//if taxpayer reaches the higher level
+			else
+			{
+				higherTaxLevel = earnings - TAX_BREAKING_POINT;
+				tax = ((float) higherTaxLevel * TAX_HIGHER) + ((float) TAX_BREAKING_POINT * TAX_LOWER);
+				System.out.printf("Tax for taxpayer %d was %.2f.%n%n",  taxPayerCounter, tax );
+			}
+		} // exit loop
 	}
 }
