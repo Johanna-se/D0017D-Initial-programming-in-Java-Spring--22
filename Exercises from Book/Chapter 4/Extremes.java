89f49d930f718cc1944d6f2b4f29975a055f7b6b
Johanna Petersson <jojanna.petersson@hotmail.com>
2022-03-09 09:08:27 +0100

    started book chapter 4

--- Exercises from Book/Chapter 4/Extremes.java
+++ Exercises from Book/Chapter 4/Extremes.java
@@ -26,5 +26,44 @@ class Extremes
 {
 	public static void main(String[] args)
 	{
+		// create Scanner to obtain input from command window
+		Scanner input = new Scanner(System.in);
+
+		//initiate variabls
+		int max = -10000;
+		int min = 10000;
+		int sum;
+		int number;
+		int howMany;
+
+		// processing phase
+		// Ask user for how many integers to add.
+		System.out.print ("How main integers are to be enterd? ");
+		howMany = input.nextInt();
+
+		//loop for entering numbers and finding max and min value
+		for (int counter = 1; counter <= howMany ; counter++)
+		{
+			System.out.print ("Enter next integer (0 - 1000): ");
+			number = input.nextInt();
+
+			//Check if the number is larger than the previos max, if so store number in max
+			if (number > max)
+			{
+			max = number;
+			}
+
+			//Check if the number is smaller than the previos min, if so store number in min
+			if (number < min)
+			{
+			min = number;
+			}
+		}//exit loop
+
+		//calculate sum with the values from the loop.
+		sum = max + min;
+
+		//print results
+		System.out.println ("The sum of the max " + max + " and min " + min + " is: " + sum);
 	}
 }
---
+++ Exercises from Book/Chapter 4/IntegersDivisibleByTre.java
@@ -0,0 +1,37 @@
+/**
+*Exercise 4.12 From the book:
+* Write an application that calculates the sum of those integers between
+* 1 and 30 that are divisible by 3.
+*
+* pseudocode:
+* initiate constant RANGE = 30
+* initiate varibles sum = 0;
+* for (int number = 1; number <= RANGE ; number++)
+*	if (number%3 == 0)
+*		sum = sum + number;
+* print sum
+*/
+
+class IntegersDivisibleByTre
+{
+	public static void main(String[] args)
+	{
+		//initiate constants
+		int RANGE = 30;
+    
+		//initiate variabls
+		int sum = 0;
+
+		// processing phase
+		for (int number = 1; number <= RANGE ; number++)
+		{
+			if (number % 3 == 0)
+			{
+				sum = sum + number;
+			}
+		}
+		
+		//return to user
+		System.out.println (sum);
+	}
+}
